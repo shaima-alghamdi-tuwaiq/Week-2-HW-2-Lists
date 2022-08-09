2223f93b9d51d86ce9f1f9e3ef166362aa93dfa6
Shaima Alghamdi <110332003+shaima-alghamdi-tuwaiq@users.noreply.github.com>
2022-08-09 14:47:39 +0300

    Create Lists.kt

---
+++ Lists.kt
@@ -0,0 +1,51 @@
+
+fun main() {
+    
+    // Read-only (immutable)
+    val names = listOf("Ahad", "Turki", "Raghad", "Nasser", "Sumayah", "Fahdah")
+    val namesAll = listOf("Ahad", "Turki", "Raghad", "Nasser")
+    val color = listOf<String>()
+    
+    // Read and Write (mutable)
+    val numbers = mutableListOf("one", "two", "three", "four", "five")
+    val weekDays = mutableListOf("Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat")
+
+    
+    // Read-Write (mutable) List - Operations
+    
+    
+    // Add (Element)
+   	
+    // Add (Index, Element)
+   	
+    // Add All (List)
+    // Add All (Index, List)
+    
+    // Clear 
+   	
+    // Remove
+    
+    // Remove At 
+    
+    // Remove All
+   	
+    
+    // Read-only (immutable) List - Operations
+     
+     
+    // The size of the list
+    
+    // Contains
+   	
+    // Contains All
+   	
+    // get
+   	
+    // indexOf
+    
+    // isEmpty
+    
+    // Sublist
+    
+    
+}
